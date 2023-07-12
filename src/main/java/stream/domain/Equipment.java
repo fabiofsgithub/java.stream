c93418edcef1baf5a8be854e22c49d269fe24fd2
fabiosilva <vivoinfo.go@gmail.com>
2023-07-12 18:32:48 -0300

    change classes by records when possible

--- src/main/java/stream/domain/Equipment.java
+++ src/main/java/stream/domain/Equipment.java
@@ -1,2 +1,23 @@
-package stream.domain;public class Equipment {
+package stream.domain;
+
+public record Equipment(Integer id, String name, Double price, EquipmentTypeEnum type) {
+
+    public Equipment(Integer id, String name, Double price) {
+        this(id, name, price, EquipmentTypeEnum.GENERAL);
+    }
+
+    public Equipment(Integer id, String name, Double price, EquipmentTypeEnum type) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipment{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
